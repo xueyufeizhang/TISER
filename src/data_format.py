fa44b18e7a04742a37ce4505d48fa5d220edf98f
xueyufeizhang <s336472@studenti.polito.it>
2025-12-21 15:31:06 +0100

    Update data_format.py

--- src/data_format.py
+++ src/data_format.py
@@ -6,11 +6,12 @@ output_file = 'data/TISER_formatted_train.jsonl'
 
 def format_tiser_data_simple(input_path):
     formatted_data = []
-    print(f"正在读取 {input_path} ...")
+    print(f"Loading {input_path} ...")
 
     try:
         with open(input_file, 'r') as f:
             for i, line in enumerate(f):
+            # for line in f:
                 line = line.strip()
                 if not line: continue
 
@@ -19,6 +20,15 @@ def format_tiser_data_simple(input_path):
                     system_text = entry.get('prompt', "")
                     user_text = entry.get('question', "")
                     assistant_text = entry.get('output', "")
+
+                    sample = {
+                        "messages": {
+                            {"role": "system", "content": system_text},
+                            {"role": "user", "content": user_text},
+                            {"role": "assistant", "content": assistant_text}
+                        }
+                    }
+                    formatted_data.append(sample)
             
                 except json.JSONDecodeError:
                     print(f"Worning: Fail to read line No.{i+1}.")
@@ -32,4 +42,24 @@ def format_tiser_data_simple(input_path):
 
 
 if __name__ == "__main__":
-    format_tiser_data_simple(input_file)
+    processed = format_tiser_data_simple(input_file)
+
+    if processed:
+        print(f"Format completed! {len(processed)} data are saved in total.")
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            for item in processed:
+                f.write(json.dumps(item, ensure_ascii=False) + '\n')
+        
+        print(f"File already saved at: {output_file}")
+
+        print("\n=== Formatted data preview (Check this!) ===")
+        sample_msg = processed[0]['messages']
+        print(f"[System]: {sample_msg[0]['content'][:50]}...")
+        print(f"[User]: {sample_msg[1]['content'][:100]}...")
+        print(f"[Assistant]: {sample_msg[2]['content'][:200]}...")
+
+    else:
+        print("Fail to format, please check file name.")
+
+    
